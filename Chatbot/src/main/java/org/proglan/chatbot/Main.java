2e939d3b0f3f1f6558dac39c6fb56049169449d0
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2023-12-08 18:50:10 +0700

    Added splash screen.

--- Chatbot/src/main/java/org/proglan/chatbot/Main.java
+++ Chatbot/src/main/java/org/proglan/chatbot/Main.java
@@ -1,11 +1,16 @@
 package org.proglan.chatbot;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
@@ -15,10 +20,35 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("chat-ui.fxml")));
-        primaryStage.setTitle("Chatbot Application");
-        primaryStage.setScene(new Scene(root, 400, 600));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent splashRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("splash-screen.fxml")));
+            Parent rootMain = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("chat-ui.fxml")));
+
+            StackPane root = new StackPane(rootMain, splashRoot);
+
+            Scene scene = new Scene(root, 400, 600);
+            primaryStage.setTitle("ChatBot");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            PauseTransition delay = getPauseTransition(splashRoot, root);
+            delay.play();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private PauseTransition getPauseTransition(Parent splashRoot, StackPane root) {
+        Duration splashDuration = Duration.seconds(2);
+
+        FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1), splashRoot);
+        fadeSplash.setFromValue(1.0);
+        fadeSplash.setToValue(0.0);
+        fadeSplash.setOnFinished(event -> root.getChildren().remove(splashRoot));
+
+        PauseTransition delay = new PauseTransition(splashDuration);
+        delay.setOnFinished(event -> fadeSplash.play());
+        return delay;
     }
 }
